0f258f33f71784c4a057d30d598f5a695c30ffa8
licetrosales <licetrosales@gmail.com>
2023-03-05 20:04:45 +0100

    Add middleware status-code 404

--- server/index.js
+++ server/index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send("Hello World");
+});
+//No route found handler
+app.use((req, res, next) => {
+    res.status(404);
+    res.json({
+        message: 'Error. Route not found'
+    });
 });
 
 module.exports = app;
