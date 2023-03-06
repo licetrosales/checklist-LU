9ea7e660f36155bae2e55c0b2071baa6dd1614b9
licetrosales <licetrosales@gmail.com>
2023-03-06 17:23:00 +0100

    Add Winston to create a log system

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+// server/index.js
+
 const express = require('express');
 
+const logger = require('./config/logger');
 const app = express();
 
 
@@ -10,18 +13,26 @@ app.get('/', (req, res, next) => {
 });
 //No route found handler
 app.use((req, res, next) => {
-    res.status(404);
+    const message = 'Route not found';
+    const statusCode = 404;
+
+    logger.warn(message);
+
+    res.status(statusCode);
     res.json({
-        message: 'Error. Route not found'
+        message,
     });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-   const {statusCode = 500, message } = err;
+   const {statusCode = 500, message} = err;
+
+   logger.error(message);
 
    res.status(statusCode);
-   res.json({message,
+   res.json({
+       message,
    });
 });
 
